336112d6ae3862d49716a313b02ab141f590f8b2
Victor Fazekas <victorfazekas3@gmail.com>
2024-10-18 17:00:20 -0300

    changes made

--- src/app/(private)/schedule/page.tsx
+++ src/app/(private)/schedule/page.tsx
@@ -3,6 +3,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { db } from "@/drizzle";
 import { auth } from "@clerk/nextjs/server";
 
+export const revalidate = 0;
+
 export default async function SchedulePage() {
   const { userId, redirectToSignIn } = auth();
   if (userId == null) return redirectToSignIn();
--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -1,12 +1,12 @@
-export function formatEventDescription(durantionInMinutes: number) {
-  const hours = Math.floor(durantionInMinutes / 60);
-  const minutes = durantionInMinutes % 60;
-  const minutesString = `${minutes} ${minutes > 1 ? "mins" : "min"}`;
-  const hoursString = `${hours} ${hours > 1 ? "hrs" : "hr"}`;
+export function formatEventDescription(durationInMinutes: number) {
+  const hours = Math.floor(durationInMinutes / 60)
+  const minutes = durationInMinutes % 60
+  const minutesString = `${minutes} ${minutes > 1 ? "mins" : "min"}`
+  const hoursString = `${hours} ${hours > 1 ? "hrs" : "hr"}`
 
-  if (hours === 0) return minutesString;
-  if (minutes === 0) return hoursString;
-  return `${hoursString} ${minutesString}`;
+  if (hours === 0) return minutesString
+  if (minutes === 0) return hoursString
+  return `${hoursString} ${minutesString}`
 }
 
 export function formatTimezoneOffset(timezone: string) {
@@ -15,5 +15,30 @@ export function formatTimezoneOffset(timezone: string) {
     timeZoneName: "shortOffset",
   })
     .formatToParts(new Date())
-    .find((part) => part.type == "timeZoneName")?.value;
+    .find(part => part.type == "timeZoneName")?.value
+}
+
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "medium",
+})
+
+export function formatDate(date: Date) {
+  return dateFormatter.format(date)
+}
+
+const timeFormatter = new Intl.DateTimeFormat(undefined, {
+  timeStyle: "short",
+})
+
+export function formatTimeString(date: Date) {
+  return timeFormatter.format(date)
+}
+
+const dateTimeFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "medium",
+  timeStyle: "short",
+})
+
+export function formatDateTime(date: Date) {
+  return dateTimeFormatter.format(date)
 }
