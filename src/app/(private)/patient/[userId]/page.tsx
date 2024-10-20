d12abcf2243ef14978c18a1e5afae0d554eddccd
Victor Fazekas <victorfazekas3@gmail.com>
2024-10-19 21:14:56 -0300

    patient page

--- src/app/(private)/patient/[userId]/page.tsx
+++ src/app/(private)/patient/[userId]/page.tsx
@@ -1,5 +1,14 @@
 import { db } from "@/drizzle";
 import { calculateAge } from "@/lib/formatters";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { FilePlus, FileSearch } from "lucide-react";
 
 export default async function PatientPage({
   params,
@@ -13,10 +22,46 @@ export default async function PatientPage({
   if (!patientInfo) return <div>Paciente não encontrado</div>;
 
   return (
-    <div>
-      <p>{patientInfo?.name}</p>
-      <p>{patientInfo?.cpf}</p>
-      <p>{calculateAge(patientInfo.dateOfBirth.toISOString())}</p>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Paciente : {patientInfo.name}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div>CPF: {patientInfo.cpf}</div>
+        <div>
+          Data de Nascimento:{" "}
+          {new Date(patientInfo.dateOfBirth).toLocaleDateString()}
+        </div>
+        <div>Idade: {calculateAge(patientInfo.dateOfBirth.toISOString())}</div>
+        <div>Endereço: {patientInfo.address}</div>
+        <div>Email: {patientInfo.email}</div>
+      </CardContent>
+      <CardFooter className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-6">
+        <Button>
+          <FilePlus size={28} />
+          <span>Relatório médico</span>
+        </Button>
+        <Button>
+          <FilePlus size={28} />
+          <span>Receita simples</span>
+        </Button>
+        <Button>
+          <FilePlus size={28} />
+          <span>Receita controlada</span>
+        </Button>
+        <Button>
+          <FilePlus size={28} />
+          <span>Atestado médico</span>
+        </Button>
+        <Button>
+          <FilePlus size={28} />
+          <span>Pedido de exame</span>
+        </Button>
+        <Button>
+          <FileSearch size={28} />
+          <span>Histórico</span>
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
