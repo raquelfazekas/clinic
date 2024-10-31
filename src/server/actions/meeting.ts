a76ccac92bfddc296eec004f31ae761d3d0b033e
Victor Fazekas <victorfazekas3@gmail.com>
2024-10-31 15:29:31 -0300

    ok

--- src/server/actions/meeting.ts
+++ src/server/actions/meeting.ts
@@ -5,7 +5,7 @@ import "use-server"
 import { z } from "zod"
 import { createCalendarEvent } from "../googleCalendar"
 import { redirect } from "next/navigation"
-import {  toZonedTime } from "date-fns-tz"
+import { fromZonedTime } from "date-fns-tz"
 import { db } from "@/drizzle"
 
 export async function createMeeting(
@@ -28,9 +28,9 @@ export async function createMeeting(
 
     if (event == null) return { error: true }
 
-    const startInTimezone = toZonedTime(data.startTime, data.timezone)
+    const startInTimezone = fromZonedTime(data.startTime, data.timezone)
 
-    console.log("startInTimezone", startInTimezone)
+    console.log("startInTimezone",startInTimezone)
 
     const validTimes = await getValidTimesFromSchedule([startInTimezone], event)
 
