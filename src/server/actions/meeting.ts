cec300f0b1a835811caf558e82fc0c19e092c983
Victor Fazekas <victorfazekas3@gmail.com>
2024-11-01 09:18:16 -0300

    changes made

--- src/server/actions/meeting.ts
+++ src/server/actions/meeting.ts
@@ -1,20 +1,19 @@
-"use server"
-import { getValidTimesFromSchedule } from "@/lib/getValidTimesFromSchedule"
-import { meetingActionSchema } from "@/schema/meetings"
-import "use-server"
-import { z } from "zod"
-import { createCalendarEvent } from "../googleCalendar"
-import { redirect } from "next/navigation"
-import { fromZonedTime } from "date-fns-tz"
-import { db } from "@/drizzle"
-import { subHours } from "date-fns"
+"use server";
+import { getValidTimesFromSchedule } from "@/lib/getValidTimesFromSchedule";
+import { meetingActionSchema } from "@/schema/meetings";
+import "use-server";
+import { z } from "zod";
+import { createCalendarEvent } from "../googleCalendar";
+import { redirect } from "next/navigation";
+import { toZonedTime } from "date-fns-tz";
+import { db } from "@/drizzle";
 
 export async function createMeeting(
   unsafeData: z.infer<typeof meetingActionSchema>
 ) {
-  const { success, data } = meetingActionSchema.safeParse(unsafeData)
+  const { success, data } = meetingActionSchema.safeParse(unsafeData);
 
-  if (!success) return { error: true }
+  if (!success) return { error: true };
 
   const event = await db.query.EventTable.findFirst({
     where: ({ clerkUserId, isActive, id }, { eq, and }) =>
@@ -23,25 +22,25 @@ export async function createMeeting(
         eq(clerkUserId, data.clerkUserId),
         eq(id, data.eventId)
       ),
-  })
+  });
 
-  if (event == null) return { error: true }
+  if (event == null) return { error: true };
 
-  const startInTimezone = fromZonedTime(subHours(data.startTime, 3), data.timezone)
+  const startInTimezone = toZonedTime(data.startTime, data.timezone);
 
-  const validTimes = await getValidTimesFromSchedule([startInTimezone], event)
-  if (validTimes.length === 0) return { error: true }
+  const validTimes = await getValidTimesFromSchedule([startInTimezone], event);
+  if (validTimes.length === 0) return { error: true };
 
   await createCalendarEvent({
     ...data,
     startTime: startInTimezone,
     durationInMinutes: event.durationInMinutes,
     eventName: event.name,
-  })
+  });
 
   redirect(
     `/book/${data.clerkUserId}/${
       data.eventId
     }/success?startTime=${data.startTime.toISOString()}`
-  )
+  );
 }
